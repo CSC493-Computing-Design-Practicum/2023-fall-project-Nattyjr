670883dbab389c63b77668856c1a3409f58e977b
Nattyjr <88628695+Nattyjr@users.noreply.github.com>
2023-10-01 21:25:23 -0400

    Click function on navbar

--- codebase/Navbar.js
+++ codebase/Navbar.js
@@ -1,36 +1,46 @@
-﻿import { Component } from "react";
+import { Component } from "react";
 import "./NavbarStyles.css";
 import { MenuItems } from "./MenuItems";
 import { Link } from "react-router-dom";
 
 class Navbar extends Component {
-    render() {
-        return (
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo">Negus</h1>
+  state = { clicked: false };
+  handleClick = () => {
+    this.setState({ clicked: !this.state.clicked })
+  }
+  render() {
+    return (
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">Negus ንጉስ</h1>
+        <div className="menu-icons" onClick={this.handleClick}>
+          <i
+            className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+          ></i>
+        </div>
 
-                <ul className="nav-menu">
-                    {MenuItems.map((item, index) => {
-                        return (
-                            <li key={index}>
-                                <a className={item.cName} href="/">
-                                    <i></i>
-                                    <i className={item.icon}></i>
-                                    {item.title}
-                                </a>
-                            </li>
-                        );
-                    })}
+        <ul className="nav-menu">
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a className={item.cName} href="/">
+                  <i></i>
+                  <i className={item.icon}></i>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+          <button>Sign Up</button>
 
-                    <li>
-                        <a href="/">
-                            <i></i>
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        );
-    }
+          <li>
+            <a href="/">
+              <i></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
