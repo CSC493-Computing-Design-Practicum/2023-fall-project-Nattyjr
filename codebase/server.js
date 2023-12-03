848746c7a7704317223908ffb9589833e3de694b
Nattyjr <88628695+Nattyjr@users.noreply.github.com>
2023-12-03 18:08:33 -0500

    Server update

--- codebase/server.js
+++ codebase/server.js
@@ -1,17 +1,41 @@
-const express = require('express')
-const mysql = require('mysql')
-const cors = require('cors')
-const app = express ()
+const express = require('express');
+const mongoose = require('mongoose');
+const cors= require("cors")
+const UserModel= require('./models/User')
 
 
+const app = express();
+app.use(express.json())
+const port = 4000;
 
-app.use(cors())
-/*app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "userTwo", "userThreee"]})
-})*/
+// MongoDB connection URI (replace with your own URI)
+const dbURI = 'mongodb://localhost:27017/Negus';
 
+// Connect to MongoDB
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
+// Check for successful connection
+const db = mongoose.connection;
+db.on('error', (error) => console.error('MongoDB connection error:', error));
+db.once('open', () => console.log('Connected to MongoDB'));
 
-app.listen(5000, () => { console.log("Server started on port 5000")})
+// Define routes or other middleware here...
+app.get('/signup', (req, res) => {
+  UserModel.create(req.body)
+  .then(users => res.json(users))
+  .catch(err => res.json(err))
+})
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 
+// Start the server
+app.listen(4000, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
