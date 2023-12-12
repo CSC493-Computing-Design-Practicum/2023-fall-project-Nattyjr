95c01b0b8bdec341b454948a1c82f7c662441174
Nattyjr <88628695+Nattyjr@users.noreply.github.com>
2023-12-12 02:43:53 -0500

    FounderInfo

---
+++ codebase/FounderInfo.js
@@ -0,0 +1,24 @@
+// FounderInfo.js
+import React from 'react';
+
+function FounderInfo() {
+  return (
+    <div className="founder-info">
+      <h2>Founder: Nate</h2>
+      <p>
+        Once upon a time, in the vibrant city of Addis Ababa, Ethiopia, a young and ambitious individual named Nate dreamed of pursuing higher education in the United States. Nate was not only passionate about expanding his knowledge but also eager to experience a new culture and build connections that transcended borders.
+      </p>
+      <p>
+        In the spring of 2021, Nate's dreams took flight as he embarked on a life-changing journey to the U.S. Armed with determination, resilience, and an insatiable curiosity, Nate arrived on American soil as an international student ready to embrace the challenges and opportunities that lay ahead.
+      </p>
+      <p>
+        As the seasons changed, so did Nate's life. Adjusting to a new academic environment, Nate found himself immersed in a world of diverse perspectives and endless possibilities. The bustling campus became his second home, and the camaraderie he shared with fellow students from around the globe formed a tapestry of friendships that would last a lifetime.
+      </p>
+      <p>
+        Connect with Nate on : <a href="https://www.linkedin.com/in/natinael-abebe-414b67214/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+      </p>
+    </div>
+  );
+}
+
+export default FounderInfo;
